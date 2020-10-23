511ac8ce34263f4c6c447c2b448f3eaaa41f8af7
Brais <brais.cotelo@udc.es>
2020-10-23 23:39:12 +0200

    O ejercicio 3 funciona excepto o equals() e o hashCode()

--- src/e3/Clock.java
+++ src/e3/Clock.java
@@ -2,20 +2,15 @@ package e3;
 import java.time.Period;
 import java.lang.IllegalArgumentException;
 public class Clock {
-    public enum Period{
-        AM, PM
-    }
+    public enum Period{AM, PM}
     public Period periodo;
     int horas, minutos, segundos;
-
     public Period getPeriodo() {
         return periodo;
     }
-
     public void setPeriodo(Period periodo) {
         this.periodo = periodo;
     }
-
     /**
      * Creates a Clock instance parsing a String .
      * @param s The string representing the hour in 24h format (17:25:15) or in
@@ -30,7 +25,6 @@ public class Clock {
             }
         }
         String[] parts = s.split(":");
-
         if(s.length() < 8 || s.length() > 11) {
             throw new IllegalArgumentException("The String is not a valid hour");
         }
@@ -43,13 +37,16 @@ public class Clock {
             horas = Integer.parseInt(parts[0]);
             minutos = Integer.parseInt(parts[1]);
             segundos = Integer.parseInt(parts[2]);
-            //periodo = parts[3];
+            if(parts[3].equals("AM")){
+                setPeriodo(Period.AM);
+            }else{
+                setPeriodo(Period.PM);
+            }
         }
         else {
             throw new IllegalArgumentException("The String is not a valid hour");
         }
     }
-
     /**
      * Creates a clock given the values in 24h format .
      * @param hours Hours in 24h format .
@@ -58,20 +55,18 @@ public class Clock {
      * @throws IllegalArgumentException if the values do not represent a valid
      * hour .
      */
-
     public Clock(int hours, int minutes, int seconds){
         if((hours < 0)||(hours > 23)||(minutes < 0)||(minutes > 59)||(seconds < 0)||(seconds > 59)) {
             throw new IllegalArgumentException("The value is not a valid hour");
         }else{
             int resultHours = hours;
             int resultMin = minutes;
-            int resultSec = seconds;
+            int resultSeg = seconds;
             minutos = resultMin;
             horas = resultHours;
-            segundos = resultSec;
+            segundos = resultSeg;
         }
     }
-
     /**
      * Creates a clock given the values in 12h format . Period is a enumeration
      * located inside the Clock class with two values : AM and PM.
@@ -82,43 +77,44 @@ public class Clock {
      * @throws IllegalArgumentException if the values do not represent a valid
      * hour .
      */
-
     public Clock(int hours, int minutes, int seconds, Period period){
-        if((hours < 0)||(hours > 12)||(minutes < 0)||(minutes > 59)||(seconds < 0)||(seconds > 59)||(!period.toString().equals("AM"))||(!period.toString().equals("PM"))) {
+        if((hours < 0)||(hours > 12)||(minutes < 0)||(minutes > 59)||(seconds < 0)||(seconds > 59) || (!period.toString().equals("AM") && !period.toString().equals("PM")) ) {
             throw new IllegalArgumentException("The value is not a valid hour");
         }else{
             int resultHours = hours;
             int resultMin = minutes;
-            int resultSec = seconds;
+            int resultSeg = seconds;
             minutos = resultMin;
             horas = resultHours;
-            segundos = resultSec;
-            //periodo = ;
+            segundos = resultSeg;
+            if(period.toString().equals("AM")){
+                setPeriodo(Period.AM);
+            }else{
+                setPeriodo(Period.PM);
+            }
         }
     }
-
     /**
      * Returns the hours of the clock in 24h format
      * @return the hours in 24h format
      */
-
     public int getHours24(){
-        if(periodo.equals(Period.PM)){
-            return horas + 12;
+        if(String.valueOf(periodo).equals("PM")){
+            int resultHours = (horas + 12);
+            return resultHours;
         }
-        else if((periodo.equals(Period.AM))&&(horas == 12)){
-            return 0;
+        else if((String.valueOf(periodo).equals("AM"))&&(horas == 12)){
+            int resultHours = 0;
+            return resultHours;
         }
         else {
             return horas;
         }
     }
-
     /**
      * Returns the hours of the clock in 12h format
      * @return the hours in 12h format
      */
-
     public int getHours12(){
         if(horas < 13){
             return horas;
@@ -127,50 +123,83 @@ public class Clock {
             return horas - 12;
         }
     }
-
     /**
      * Returns the minutes of the clock
      * @return the minutes
      */
-
     public int getMinutes(){
         return minutos;
     }
-
     /**
      * Returns the seconds of the clock .
      * @return the seconds .
      */
-
     public int getSeconds(){
         return segundos;
     }
-
     /**
      * Returns the period of the day (AM or PM) that the clock is representing
      * @return An instance of the Clock . Period enum depending if the time is
      * before noon (AM) or after noon (PM ).
      */
-
-    public int getPeriod(){
+    public Period getPeriod(){
+        if(periodo != null){
+            return periodo;
+        }
+        if(horas >= 12){
+            periodo = Period.PM;
+            return periodo;
+        }else{
+            periodo = Period.AM;
+            return periodo;
+        }
     }
-
     /**
      * Prints a String representation of the clock in 24h format .
      * @return An string in 24h format
      * @see String . format function to format integers with leading zeroes
      */
-
     public String printHour24(){
+        String str = "";
+        horas = this.getHours24();
+        String sHoras = String.valueOf(horas);
+        String sMinutos = String.valueOf(minutos);
+        String sSegundos = String.valueOf(segundos);
+        if(sHoras.length() == 1){
+            sHoras = "0".concat(sHoras);
+        }
+        if(sMinutos.length() == 1){
+            sMinutos = "0".concat(sMinutos);
+        }
+        if(sSegundos.length() == 1){
+            sSegundos = "0".concat(sSegundos);
+        }
+        str = str.concat(sHoras + ":" + sMinutos + ":" + sSegundos);
+        return str;
     }
-
     /**
      * Prints a String representation of the clock in 12h format .
      * @return An string in 12h format
      * @see String . format function to format integers with leading zeroes
      */
-
     public String printHour12(){
+        String str = "";
+        getPeriod();
+        horas = this.getHours12();
+        String sHoras = String.valueOf(horas);
+        String sMinutos = String.valueOf(minutos);
+        String sSegundos = String.valueOf(segundos);
+        if(sHoras.length() == 1){
+            sHoras = "0".concat(sHoras);
+        }
+        if(sMinutos.length() == 1){
+            sMinutos = "0".concat(sMinutos);
+        }
+        if(sSegundos.length() == 1){
+            sSegundos = "0".concat(sSegundos);
+        }
+        str = str.concat(sHoras + ":" + sMinutos + ":" + sSegundos + " " + periodo);
+        return str;
     }
 
     /**
@@ -183,6 +212,20 @@ public class Clock {
 
     @Override
     public boolean equals(Object o){
+        periodo = this.getPeriod();
+        horas = this.getHours24();
+        if(this == o) {
+            return true;
+        }
+        else if(o == null){
+            return false;
+        }
+        else if(this.getClass() == o.getClass()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /**
@@ -194,6 +237,12 @@ public class Clock {
 
     @Override
     public int hashCode(){
+        int result;
+        result = horas;
+        result = 31 * result + minutos;
+        result = 31 * result + segundos;
+        result = 31 * result + (String.valueOf(periodo).hashCode());
+        return result;
     }
 
 }
